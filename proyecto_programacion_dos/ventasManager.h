eeac5b6527c6abd481dcb033344383761d909366
kevn-gonzalez <kevin-gonzalez@hotmail.com.ar>
2025-06-13 00:22:23 -0300

    Update ventasManager.h

--- proyecto_programacion_dos/ventasManager.h
+++ proyecto_programacion_dos/ventasManager.h
@@ -1,6 +1,26 @@
 #ifndef VENTASMANAGER_H_INCLUDED
 #define VENTASMANAGER_H_INCLUDED
 
+/**
 
+class ventasManager
+{
+    public:
+        Venta obj;
+        VentaArchivo archVenta;
+    private:
+    VentasManager();
+
+    void submenuListarVentas();
+    void submenuImporteTotal();
+    void registrarVenta();
+    void cancelarVenta();
+    void buscarVentas();
+    void mostrarVenta();
+    void guardarCambios();
+};
+
+#endif // VENTASMANAGER_H_INCLUDED
+*/
 
 #endif // VENTASMANAGER_H_INCLUDED
