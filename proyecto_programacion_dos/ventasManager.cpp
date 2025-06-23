9a8fb322ed11a6c4a3bb176fb14214a62a864157
kevn-gonzalez <kevin-gonzalez@hotmail.com.ar>
2025-06-22 23:09:19 -0300

    ventasporhora

--- proyecto_programacion_dos/ventasManager.cpp
+++ proyecto_programacion_dos/ventasManager.cpp
@@ -423,10 +423,49 @@ void ventasManager::submenuListarVentas(){
                 }
                 break;
             case 3:
-                {
+                {                
+                    system("cls");
+                    Fecha _fecha;
+                    int hora = 0;
+                    int acu = 0;
+                    bool correcto = false;
+                    while(correcto == false)
+                        {
+                        cout << "Ingrese la hora: " ;
+                        cin >> hora;
 
+
+                        if(hora >= 00 && hora <= 23)
+                            {
+                            correcto = true;
+                            }
+                        else
+                            {
+                            cout << "La hora ingresada es incorrecta, por favor ingrese un horario nuevamente";
+                            system ("pause");
+                            }
+                        }
+
+                    for(int z = 0 ; z < cantidad ; z++)
+                        {
+                        obj = archVenta.leerVenta(z);
+                        if(obj.getFechaProyeccion().getHora() == hora)
+                            {
+                            mostrarVenta(obj);
+                            acu++;
+                            }
+                        }
+                    if (acu == 0)
+                        {
+                        cout << endl << "=================================================" << "NO HUBO VENTAS EN TAL HORARIO" << endl;
+                        }
+                    else
+                        {
+                        cout << endl << "=================================================" << endl << "HUBO UN TOTAL DE " << acu << " VENTAS EN EL HORARIO DE LAS " << hora << ":00 hs. APROX." << endl;
+                        }
                 }
-                break;
+                system("pause");
+                break;        
             case 4:
                 {
                     return;
@@ -448,86 +487,4 @@ void ventasManager::submenuListarVentas(){
     }while(finProceso != 0);
 }
 
-/*
-void VentasManager::submenuListarVentas() {
-    if (listaVentas.empty()) {
-        cout << "No hay ventas registradas." << endl;
-        return;
-    }
 
-    cout << "Listado de Ventas:" << endl;
-    for (const auto& venta : listaVentas) {
-        cout << venta.mostrarInformacion() << endl;
-    }
-}
-
-void VentasManager::submenuImporteTotal() {
-    double total = 0.0;
-    for (const auto& venta : listaVentas) {
-        total += venta.getPrecioTotal();
-    }
-    cout << "El importe total de las ventas es: $" << total << endl;
-}
-
-void VentasManager::registrarVenta() {
-    int idSala, idPelicula, cantidadAsientos;
-    int asientosVendidos[] = {};
-    double precioTotal;
-    string fechaHora;
-
-    cout << "Ingrese el ID de la sala: ";
-    cin >> idSala;
-
-    cout << "Ingrese el ID de la película: ";
-    cin >> idPelicula;
-
-    cout << "Ingrese la cantidad de asientos vendidos: ";
-    cin >> cantidadAsientos;
-
-    for (int i = 0; i < cantidadAsientos; i++) {
-        int asiento;
-        cout << "Ingrese el número del asiento vendido: ";
-        cin >> asiento;
-        asientosVendidos.guardarventa();
-    }
-
-    cout << "Ingrese el precio total de la venta: ";
-    cin >> precioTotal;
-
-    cout << "Ingrese la fecha y hora de la venta (ej: 2024-01-26 19:30): ";
-    cin.ignore();
-    getline(std::cin, fechaHora);
-
-    // Crear el objeto venta
-}
-
-void VentasManager::cancelarVenta() {
-    int idVenta;
-    cout << "Ingrese el ID de la venta a cancelar: ";
-    cin >> idVenta;
-
-    // Buscar la venta por ID
-
-}
-
-void VentasManager::buscarVentas() {
-    int idVenta;
-    cout << "Ingrese el ID de la venta a buscar: ";
-    cin >> idVenta;
-
-}
-
-void VentasManager::mostrarVenta() {
-    int idVenta;
-    cout << "Ingrese el ID de la venta a mostrar: ";
-    cin >> idVenta;
-
-}
-
-void VentasManager::guardarCambios() {
-    for (const auto& venta : listaVentas) {
-        ventasArchivo.guardarVenta(venta);
-    }
-    std::cout << "Cambios guardados exitosamente en el archivo." << std::endl;
-}
-*/
