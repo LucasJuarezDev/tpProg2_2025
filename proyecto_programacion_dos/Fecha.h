a96568af9bb8e077b430a7b6ac3aaf40748517f5
kevn-gonzalez <kevin-gonzalez@hotmail.com.ar>
2025-06-22 23:13:44 -0300

    agregue hora

--- proyecto_programacion_dos/Fecha.h
+++ proyecto_programacion_dos/Fecha.h
@@ -7,16 +7,18 @@ using namespace std;
 class Fecha
 {
 private:
-    int Dia, Mes, Anio;
+    int Dia, Mes, Anio, Hora;
 public:
-    Fecha(int _Dia, int _Mes, int _Anio);
+    Fecha(int _Dia, int _Mes, int _Anio, int _Hora);
     Fecha();
     int getDia();
     int getMes();
     int getAnio();
+    int getHora();
     void setDia(int _Dia);
     void setMes(int _Mes);
     void setAnio(int _Anio);
+    void setHora(int _Hora);
     void MostrarFechaActual();
     Fecha ReturnFechaActual();
     void CargarFecha();
